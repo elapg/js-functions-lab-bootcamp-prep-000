57a7212709eaeb82eb6b9aa99fa0b8a33dc60da3
Mariela Gonzales <mpgonzales14@gmail.com>
2019-03-02 02:01:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // write your code below!
+function happyHolidays() {
+return("Happy holidays!") 
+}
+
+function happyHolidaysTo(you) {
+  console.log(`${you}!`)
+  return `Happy holidays, ${you}!`
+}
+
+
+function happyHolidayTo(you, mom) {
+  return`${you}, ${mom}!`
+  
+}
+
+function holidayCountdown() {
+  return(`It's ${3} days until ${holiday}!`)
+}
+
+
+
+
+
